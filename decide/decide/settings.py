968a5dc9595c55d5d49159db6af9b52dd32da6b4
angcabdom <angcabdom@alum.us.es>
2021-02-12 10:58:53 +0000

    Cambios para docker

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -73,7 +73,12 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://egc-1230-angcabdom.herokuapp.com'
+
+APIS = {}
+
+import django_heroku
+django_heroku.settings(locals())
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
